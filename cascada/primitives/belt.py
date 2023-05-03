de1a89c5293bf0d934db5103f5171dc2664d8800
Svirid_Polina <polina.svirid.03@mail.ru>
2023-05-03 10:59:08 +0300

    bit-vector structure is completed

--- cascada/primitives/belt.py
+++ cascada/primitives/belt.py
@@ -9,7 +9,11 @@ from cascada.primitives.blockcipher import Encryption, Cipher
 
 from cascada.differential.opmodel import OpModel
 from cascada.bitvector.secondaryop import PopCount
+from cascada.differential.difference import XorDiff, RXDiff
 from cascada.bitvector import core
+from cascada.bitvector.operation import (
+    SecondaryOperation, RotateLeft, BvComp, Ite, zero_extend
+)
 
 
 class BeltInstance(enum.Enum):
@@ -24,19 +28,6 @@ def get_Belt_instance(belt_instance):
         m = 8
     else:
         raise ValueError("invalid instance of cipher")
-
-    # # Modular Substraction
-    # def modsub(x, y):
-    #     mod = 2**32
-    #     return (x - y) % mod
-    #
-    # # Modular addition
-    # def modadd(*x):
-    #     mod = 2**32
-    #     res = 0
-    #     for el in x:
-    #         res = (res + reverse(el)) % mod
-    #     return reverse(res)
 
     H = [0xB1, 0x94, 0xBA, 0xC8, 0x0A, 0x08, 0xF5, 0x3B, 0x36, 0x6D, 0x00, 0x8E, 0x58, 0x4A, 0x5D, 0xE4,
          0x85, 0x04, 0xFA, 0x9D, 0x1B, 0xB6, 0xC7, 0xAC, 0x25, 0x2E, 0x72, 0xC2, 0x02, 0xFD, 0xCE, 0x0D,
@@ -63,19 +54,6 @@ def get_Belt_instance(belt_instance):
         # [a0, a1, a2, a3]  ->  [a3, a2, a1, a0]
         return [my_word[(3 - i) % 4] for i in range(4)]
 
-    # def Add(a, b):
-    #     list_a = hex2byte_list(str(a)[2:])
-    #     print(list_a)
-    #     print(int(str(list_a[0]), 16))
-    #     list_b = hex2byte_list(str(b)[2:])
-    #     print(list_b)
-    #     x1 = (int(str(list_a[0]), 16) + int(str(list_b[0]), 16)) % 256
-    #     x2 = (int(str(list_a[1]), 16) + int(str(list_b[1]), 16)) % 256
-    #     x3 = (int(str(list_a[2]), 16) + int(str(list_b[2]), 16)) % 256
-    #     x4 = (int(str(list_a[3]), 16) + int(str(list_b[3]), 16)) % 256
-    #     print(x1, x2, x3, x4)
-    #     print(hex(x1), hex(x2), hex(x3), hex(x4))
-
     def hex2byte_list(state):
         """Convert the hexadecimal string to a byte list
 
@@ -89,9 +67,6 @@ def get_Belt_instance(belt_instance):
             byte_list.append(Constant(my_byte, 8))
         return byte_list
 
-    def  circularleftshift(value, k):
-        return RotateLeft(value, k)
-
     def gtransformation(x, k):
         list_hex = hex2byte_list(str(x)[2:])
         res = [htransformation(i) for i in list_hex]
@@ -104,6 +79,47 @@ def get_Belt_instance(belt_instance):
         string = str(res_rev[0]) + str(res_rev[1])[2:] + str(res_rev[2])[2:] + str(res_rev[3])[2:]
         res_rev = Constant(int(string, 16), 32)
         return res_rev
+
+    def int2list(x):
+        return [x >> i & 0xff for i in [24, 16, 8, 0]]
+
+    def ADD(a, b):
+        list_hex_a = hex2byte_list(str(a)[2:])
+        rev_a = rot_word(list_hex_a)
+
+        list_hex_b = hex2byte_list(str(b)[2:])
+        rev_b = rot_word(list_hex_b)
+
+        int_a = int(str(rev_a[0]), 16) * pow(2, 24) + int(str(rev_a[1]), 16) * pow(2, 16) + int(str(rev_a[2]), 16) * pow(2, 8) + int(str(rev_a[3]), 16)
+        int_b = int(str(rev_b[0]), 16) * pow(2, 24) + int(str(rev_b[1]), 16) * pow(2, 16) + int(str(rev_b[2]), 16) * pow(2, 8) + int(str(rev_b[3]), 16)
+
+        res = (int_a + int_b) % 2**32
+        res = int2list(res)
+        res = rot_word(res)
+        res_const = [Constant(res[0], 8), Constant(res[1], 8), Constant(res[2], 8), Constant(res[3], 8)]
+        string = str(res_const[0]) + str(res_const[1])[2:] + str(res_const[2])[2:] + str(res_const[3])[2:]
+        value = Constant(int(string, 16), 32)
+
+        return value
+
+    def SUB(a, b):
+        list_hex_a = hex2byte_list(str(a)[2:])
+        rev_a = rot_word(list_hex_a)
+
+        list_hex_b = hex2byte_list(str(b)[2:])
+        rev_b = rot_word(list_hex_b)
+
+        int_a = int(str(rev_a[0]), 16) * pow(2, 24) + int(str(rev_a[1]), 16) * pow(2, 16) + int(str(rev_a[2]), 16) * pow(2, 8) + int(str(rev_a[3]), 16)
+        int_b = int(str(rev_b[0]), 16) * pow(2, 24) + int(str(rev_b[1]), 16) * pow(2, 16) + int(str(rev_b[2]), 16) * pow(2, 8) + int(str(rev_b[3]), 16)
+
+        res = (int_a - int_b) % 2 ** 32
+        res = int2list(res)
+        res = rot_word(res)
+        res_const = [Constant(res[0], 8), Constant(res[1], 8), Constant(res[2], 8), Constant(res[3], 8)]
+        string = str(res_const[0]) + str(res_const[1])[2:] + str(res_const[2])[2:] + str(res_const[3])[2:]
+        value = Constant(int(string, 16), 32)
+
+        return value
 
     class BeltKeySchedule(RoundBasedFunction):
         """Key schedule function."""
@@ -145,18 +161,29 @@ def get_Belt_instance(belt_instance):
             for j in range(0, 56):
                 k[j] = cls.round_keys[j % 8]
             for i in range(cls.num_rounds):
-                b = b ^ gtransformation(a + k[7 * (i + 1) - 7], 5)
-                c = c ^ gtransformation(d + k[7 * (i + 1) - 6], 21)
-                a = a - gtransformation(b + k[7 * (i + 1) - 5], 13)
-                e = (gtransformation(b + c + k[7 * (i + 1) - 4], 21)) ^ (i + 1)
-                b = b + e
-                c = c - e
-                d = d + gtransformation(c + k[7 * (i + 1) - 3], 13)
-                b = b ^ gtransformation(a + k[7 * (i + 1) - 2], 21)
-                c = c ^ gtransformation(d + k[7 * (i + 1) - 1], 5)
+                b = b ^ gtransformation(ADD(a, k[7 * (i + 1) - 7]), 5)
+                # print("b", b)
+                c = c ^ gtransformation(ADD(d, k[7 * (i + 1) - 6]), 21)
+                # print("c", c)
+                a = SUB(a, gtransformation(ADD(b, k[7 * (i + 1) - 5]), 13))
+                # print("a", a)
+                e = (gtransformation(ADD(ADD(b, c), k[7 * (i + 1) - 4]), 21)) ^ Constant(int("0x0" + str(i+1) + "000000", 16), 32)
+                # print(e)
+                b = ADD(b, e)
+                # print("b", b)
+                c = SUB(c, e)
+                # print("c", c)
+                d = ADD(d, gtransformation(ADD(c, k[7 * (i + 1) - 3]), 13))
+                # print("d", d)
+                b = b ^ gtransformation(ADD(a, k[7 * (i + 1) - 2]), 21)
+                # print("b", b)
+                c = c ^ gtransformation(ADD(d, k[7 * (i + 1) - 1]), 5)
+                # print("c", c)
                 a, b = b, a
                 c, d = d, c
                 b, c = c, b
+
+                # print(a, b, c, d)
 
                 cls.add_round_outputs(a, b, c, d)
             return b, d, a, c
@@ -186,3 +213,58 @@ def get_Belt_instance(belt_instance):
             cls.set_num_rounds(old_num_rounds)
 
     return BeltCipher
+
+class BeltRF(SecondaryOperation):
+    """The non-linear part of the round function of Simon.
+
+    This corresponds to ``f(x) = ((x <<< a) & (x <<< b)) ^ (x <<< c)``,
+    where ``(a, b, c) = (8, 1, 2)``.
+    """
+    # a = 8
+    # b = 1
+    # c = 2
+    #
+    # arity = [1, 0]
+    # is_symmetric = False
+    #
+    # @classmethod
+    # def output_width(cls, x):
+    #     return x.width
+    #
+    # @classmethod
+    # def eval(cls, x):
+    #     return (RotateLeft(x, cls.a) & RotateLeft(x, cls.b)) ^ RotateLeft(x, cls.c)
+
+class XorModelBelt(OpModel):
+    """Represent the `XorDiff` `differential.opmodel.OpModel` of `Belt`.
+
+        >>> from cascada.bitvector.core import Constant, Variable
+        >>> from cascada.differential.difference import XorDiff
+        >>> from cascada.primitives.belt import XorModelBelt
+        >>> alpha = XorDiff(Constant(0, 32))
+        >>> f = XorModelBelt(alpha)
+        >>> print(f.vrepr())
+        XorModelBelt(XorDiff(Constant(0x00000000, width=32)))
+        >>> x = Constant(0, 32)
+        >>> f.eval_derivative(x)  # f(x + alpha) - f(x)
+        XorDiff(0x00000000)
+        >>> f.max_weight(), f.weight_width(), f.error(), f.num_frac_bits()
+
+    """
+
+    diff_type = XorDiff
+    op = BeltRF
+
+    def max_weight(self):
+        n = self.input_diff[0].val.width
+        return n - 1  # as an integer
+
+    def weight_width(self):
+        n = self.input_diff[0].val.width
+        return max((n - 1).bit_length(), PopCount.output_width(core.Variable("x", n)))
+
+    def num_frac_bits(self):
+        return 0
+
+    def error(self):
+        return 0
